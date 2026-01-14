0fa1615dae279e76305d8b429dca4ea84d362c7b
temycodes <temmy1345@gmail.com>
2026-01-14 15:09:40 +0100

    security changes

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://qhjolsyplpevjuxtdviu.supabase.co";
+export const supabaseUrl = process.env.SUPABASE_URL;
 
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoam9sc3lwbHBldmp1eHRkdml1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyNzkzMjksImV4cCI6MjA3Nzg1NTMyOX0.vQamH8C6O6VO3l9Hyu_TMrC-36aG5hSYaF_eugOsYpM";
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
